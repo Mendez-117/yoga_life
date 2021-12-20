8081e16b65f1dea87b87f625fda3cd179b2b0e4f
Mendez-117 <92990495+Mendez-117@users.noreply.github.com>
2021-12-20 21:35:43 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,15 @@
+function about() {
+    window.location='about.html';
+}
+
+function pic() {
+    window.location='sample_pic.html';
+}
+
+function vid() {
+    window.location='sample_vide.html'
+}
+
+function batch() {
+    window.location='batch.html'
+}
